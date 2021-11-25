b548c6ba6c8d75661b8578a39e2add88bf11e96a
Stanislav Reva <revasa95@gmail.com>
2021-11-25 20:31:51 +0300

    Add button component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
-import { Htag } from '../components';
+import { Button, Htag } from '../components';
 
 export default function Home(): JSX.Element {
 	return (
 		<div>
 			<Htag tag='h1'>Hello world!</Htag>
+			<Button appearance='primary'>Primary</Button>
+			<Button appearance='ghost'>Ghost</Button>
+			<Button appearance='ghost' arrow='right'>Ghost arrow right</Button>
+			<Button appearance='ghost' arrow='down'>Ghost arrow down</Button>
 		</div>
 	);
 }

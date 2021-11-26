5e6c6f85d9d58c4347b391f2982fe10e38b2465e
Stanislav Reva <revasa95@gmail.com>
2021-11-26 14:46:01 +0300

    Make footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,9 +1,18 @@
+import styles from './Footer.module.css';
+import cn from 'classnames';
+import { format } from 'date-fns';
 import { FooterProps } from './Footer.props';
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+	const currentYear: string = format(new Date(), 'yyyy');
+
 	return (
-		<div {...props}>
-			Footer
-		</div>
+		<footer className={cn(className, styles.footer)} {...props}>
+			<div>
+				OwlTop © 2021 - {currentYear} Все права защищены
+			</div>
+			<a href="#" target="_blank">Пользовательское соглашение</a>
+			<a href="#" target="_blank">Политика конфиденциальности</a>
+		</footer>
 	);
 };
